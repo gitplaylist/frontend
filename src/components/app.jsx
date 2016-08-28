99eada08ce5e8347222a36aea73e7d1342fda6c1
Tim <besttimothy1@gmail.com>
2016-08-28 15:53:09 -0700

    move main app page into a its own file

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,37 +3,11 @@ import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
 import { IndexView } from './index.jsx';
 import { AboutView } from './about.jsx';
 import { NoMatchView } from './nomatch.jsx';
+import { BaseTemplate } from './base.jsx';
 
-
-export class App extends React.Component {
-  render() {
-    return (<div>
-      <header>
-        <div className="top-bar">
-          <h1>
-            <Link to="/">gitPlaylists</Link>
-          </h1>
-          <nav>
-            <Link to="/about">Login</Link>
-            <Link to="/login">Sign up</Link>
-          </nav>
-        </div>
-      </header>
-      <main>
-        {this.props.children}
-      </main>
-      <footer className="container">
-        <hr/>
-        <span>
-          FOOTER
-        </span>
-      </footer>
-    </div>);
-  }
-}
 
 export const router = <Router history={browserHistory}>
-  <Route path="/" component={App}>
+  <Route path="/" component={BaseTemplate}>
     <IndexRoute component={IndexView} />
     <Route path="about" component={AboutView} />
     <Route path="*" component={NoMatchView} />
---
+++ src/components/base.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <div className="top-bar">
+          <h1>
+            <Link to="/">gitPlaylists</Link>
+          </h1>
+          <nav>
+            <Link to="/about">Login</Link>
+            <Link to="/login">Sign up</Link>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <footer className="container">
+        <hr/>
+        <span>BaseTemplate
+          FOOTER
+        </span>
+      </footer>
+    );
+  }
+}
+
+export class BaseTemplate extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <main>
+          {this.props.children}
+        </main>
+        <Footer/>
+      </div>
+    );
+  }
+}
